20fdd73b6b352310a144a33cdbf72b14b920354c
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-02-04 17:26:50 +0300

    Add project files.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekleme(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri eklendi:" +" " + musteri.Ad+" " +musteri.Soyad+" "+musteri.Yas);
+        }
+
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri silindi" + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas);
+        }
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi" + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas);
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ayşe";
+            musteri1.Soyad = "Korkmaz";
+            musteri1.Yas = 42;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ebru";
+            musteri2.Soyad = "Yılmaz";
+            musteri2.Yas = 34;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Hakan";
+            musteri3.Soyad = "Özdemir";
+            musteri3.Yas = 24;
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekleme(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Listeleme(musteri3);
+
+        }
+    }
+}
